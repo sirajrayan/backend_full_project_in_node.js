00744fa56a846e519c475d0359e421abb8d99ae5
siraj khan <rayansiraj88@gmail.com>
2025-09-09 09:29:03 -0700

    two ways of connecting DB

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+const connectDB= async ()=>{
+   
+    try {
+       const connectionInstance= await mongoose.connect
+        (`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+
+        console.log(`\n MONGODB connected !! HOST: 
+            ${connectionInstance.connection.host}`)
+        
+    } catch (error) {
+        console.log("MONGODB connection error :",error)
+        process.exit(1)
+    }
+}
+
+export default  connectDB
